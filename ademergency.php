a6721457d3611ce6737c02b10d95dcfdfce92ab3
danielkirwa <kirwadan03@gmail.com>
2021-11-23 23:12:14 +0300

    modified file

---
+++ ademergency.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8">
+	<title>add emergency</title>
+	<link rel="stylesheet" type="text/css" href="styles/banner.css">
+	<link rel="stylesheet" type="text/css" href="styles/shopping.css">
+</head>
+<body>
+	<div class="banner">
+		<table>
+ 			<tr>
+ 				<td>
+ 					<label class="largeText">My Expense</label>
+ 				</td>
+ 				<td>
+ 					<img src="assets/potrait.png" width="75px" class="accountAvertor">
+ 				</td>
+ 				
+ 			</tr>
+ 		</table>
+	</div>
+	<br>
+	<center>
+		<h2 class="dodgerblueText">Your emergency manager</h2>
+	</center>
+
+		
+
+ 	<div class="main-bill"> 
+ 		
+ 		
+ 			
+ 				<div class="smallmargintop">
+ 					<center><img src="assets/shopping.png" width="75px"></center>
+ 				</div>
+ 				<div class="smallmargintop">
+ 					<center>
+ 					<label class="largeText dodgerblueText">Add your emergency : <span>null</span></label> &nbsp;</center>
+ 					</div>
+<hr style="color: dodgerblue;">	
+ 	</div>
+ 	<div>
+
+
+ 	</div>
+
+
+ 	<div class="scroll-table">
+ 	<div class="table-holder">
+ 		<div class="table-caption">
+ 			<label class="largeText brownText">Active active saving   : <span>null</span></label>
+ 		</div>
+ 		<table>
+ 			<thead>
+ 				<tr>
+          <th>Savings ID</th>
+          <th>Savings name</th>
+          <th>Savings Description</th>
+ 					<th>Closing On</th>
+ 					<th>Target</th>
+ 					<th>Action</th>
+ 				</tr>
+ 			</thead>
+ 			<tbody>
+
+  
+    </tbody>
+ 		</table>
+
+  </div>
+</div>
+       
+
+</body>
+</html>
